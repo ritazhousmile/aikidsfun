4677745fa891037b808feee53c98257b5d1c39f4
ritazhousmile <ritazhousmile@gmail.com>
2019-07-30 09:24:44 -0400

    add user profile index page, upload user profile picture syling the nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
+  mount_uploader :profile_photo, ProfilePhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  available_days = ['Monday', 'Tuesday', 'Wendsday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :town, presence: true
+  validates :age, presence: true
+  validates :career, presence: true
+  validates :availability, presence: true, inclusion: {in: available_days}
 end
