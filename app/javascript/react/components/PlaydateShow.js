a1e62591ea8a242123891b3b134e218b99184d0c
ritazhousmile <ritazhousmile@gmail.com>
2019-08-01 11:23:00 -0400

    add playdate show page

---
+++ app/javascript/react/components/PlaydateShow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PlaydateShow = (props) => {
+  return(
+    <div>
+      <div className="playdate-name">
+        <h3>{props.name}</h3>
+      </div>
+      <div className="playdate-location">{props.location}</div>
+      <div className="playdate-time">{props.time}</div>
+      <div className="playdate-desc">{props.description}</div>
+    </div>
+
+  )
+}
+export default PlaydateShow;
