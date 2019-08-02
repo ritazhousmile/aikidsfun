96d379ba163126bb25d7f7a4f4f3a748fdb8729f
ritazhousmile <ritazhousmile@gmail.com>
2019-08-02 10:45:59 -0400

    delete comment out part

--- app/javascript/react/components/PlaydateShow.js
+++ app/javascript/react/components/PlaydateShow.js
@@ -4,7 +4,6 @@ const PlaydateShow = (props) => {
   return(
     <div>
       <div className="playdate-header">
-
         <div className="playdate-name">
           <h3>{props.name}</h3>
         </div>
@@ -14,26 +13,6 @@ const PlaydateShow = (props) => {
         <div className="playdate-desc">{props.description}</div>
       </div>
     </div>
-
   )
 }
 export default PlaydateShow;
-
-// <div className="playdate-host">
-//   <span class="playdate-host-pic">
-//     <%= image_tag current_user.profile_photo.url.to_s %>
-//   </span>
-//   <span>Hosted by {props.host}</span>
-// </div>
-
-
-// <div className="playdate-time">
-//   // {new Intl.DateTimeFormat('en-US', {
-//   //     year: 'numeric',
-//   //     month: 'short',
-//   //     day: 'numeric',
-//   //     weekday: 'short',
-//   //     hour: 'numeric',
-//   //     minute: '2-digit'
-//   // }).format(props.time)}
-// </div>
