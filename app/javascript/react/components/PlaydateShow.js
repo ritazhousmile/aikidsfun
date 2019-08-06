9c5240eb5eda791975be14fe2932d44d05cdd52a
ritazhousmile <ritazhousmile@gmail.com>
2019-08-06 12:42:23 -0400

    add feature that user can click attend button

--- app/javascript/react/components/PlaydateShow.js
+++ app/javascript/react/components/PlaydateShow.js
@@ -3,10 +3,26 @@ import React from 'react';
 const PlaydateShow = (props) => {
   let editButton
   let deleteButton
-  if (currentUserId == props.hostId) {
+  let attendButton
+  let attendees
+
+  if (props.currentUser.id == props.hostId) {
     editButton = <button onClick={props.handleClick}>Edit</button>
     deleteButton = <button onClick={props.handleDelete}>Delete</button>
+  } else {
+    attendButton = <button onClick={props.handleAttendClick}>Attend</button>
   }
+
+   attendees = props.attendees.map (attendee => {
+    return(
+      <div key={attendee.id} className="playdate-attendee">
+        <a href={"/users/" + attendee.id}>
+          <img className="profile-photo-round" src={attendee.profile_photo.url}></img>
+        </a>
+      </div>
+    )
+  })
+  
   return(
     <div>
       <div className="playdate-header">
@@ -14,15 +30,17 @@ const PlaydateShow = (props) => {
           <h3>{props.name}</h3>
             {editButton}
             {deleteButton}
+            {attendButton}
         </div>
       </div>
-      <div>Hosted by {props.host}</div>
+      <div>Hosted by {props.hostFirstName + ' ' + props.hostLastName}</div>
       <div>Time: {props.time}</div>
 
       <div className="playdate-details">
         <div className="playdate-location">Location: {props.location}</div>
         <div className="playdate-desc">Description: {props.description}</div>
       </div>
+      <div className="playdate-attendees">Attendees: {attendees}</div>
     </div>
   )
 }
